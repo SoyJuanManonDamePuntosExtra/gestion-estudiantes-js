44cbb797c61a61ac1a12c3543689b3067da6dfa2
SoyJuanManonDamePuntosExtra <20110008803@cafam.edu.do>
2025-03-10 22:05:32 -0300

    Mensaje del cambio

--- reportes.js
+++ reportes.js
@@ -1,19 +1,81 @@
-import { estudiantes } from "./estudiantes.js";
+import { estudiantes } from './estudiantes.js';
 
-const listarEstudiantesConArea = () => {
-  return estudiantes.map(est => ({ nombre: est.nombre, área: est.nivel }));
+export const listarEstudiantesConArea = () => {
+    return estudiantes.map(est => ({ nombre: est.nombre, área: est.nivel }));
 };
 
-const buscarEstudiante = (idONombre) => {
-  return estudiantes.find(est => est.id === idONombre || est.nombre.toLowerCase() === idONombre.toLowerCase()) || "Estudiante no encontrado";
+export const buscarEstudiante = (idONombre) => {
+    return estudiantes.find(est => est.id === idONombre || est.nombre.toLowerCase() === idONombre.toLowerCase());
 };
 
-const promedioPorEstudiante = () => {
-  return estudiantes.map(est => {
-    const calificaciones = Object.values(est.calificaciones);
-    const promedio = calificaciones.length ? calificaciones.reduce((a, b) => a + b, 0) / calificaciones.length : 0;
-    return { nombre: est.nombre, promedio: promedio.toFixed(2), área: est.nivel };
-  });
+export const promedioPorEstudiante = () => {
+    return estudiantes.map(est => {
+        const calificaciones = Object.values(est.calificaciones);
+        const promedio = calificaciones.reduce((acc, nota) => acc + nota, 0) / calificaciones.length;
+        return { nombre: est.nombre, promedio, área: est.nivel };
+    });
 };
 
-export { listarEstudiantesConArea, buscarEstudiante, promedioPorEstudiante };
+export const filtrarEstudiantesPorPromedio = (umbral) => {
+    return promedioPorEstudiante().filter(est => est.promedio > umbral);
+};
+
+export const estudiantesAprobadosReprobadosPorMateria = (materia) => {
+    const aprobados = estudiantes.filter(est => est.calificaciones[materia] >= 60)
+        .map(est => ({ nombre: est.nombre, calificación: est.calificaciones[materia], área: est.nivel }));
+    
+    const reprobados = estudiantes.filter(est => est.calificaciones[materia] < 60)
+        .map(est => ({ nombre: est.nombre, calificación: est.calificaciones[materia], área: est.nivel }));
+    
+    return { aprobados, reprobados };
+};
+
+export const promedioGeneralGrupo = () => {
+    const promedios = promedioPorEstudiante().map(est => est.promedio);
+    const promedioGeneral = promedios.reduce((acc, nota) => acc + nota, 0) / promedios.length;
+    return { promedioGeneral };
+};
+
+export const promedioPorAreaDeEstudio = (nivel) => {
+    const estudiantesDelArea = estudiantes.filter(est => est.nivel === nivel);
+    if (estudiantesDelArea.length === 0) return { [nivel]: 0 };
+
+    const promedio = estudiantesDelArea
+        .map(est => Object.values(est.calificaciones).reduce((acc, nota) => acc + nota, 0) / Object.keys(est.calificaciones).length)
+        .reduce((acc, prom) => acc + prom, 0) / estudiantesDelArea.length;
+
+    return { [nivel]: promedio };
+};
+
+export const distribucionEstudiantesPorArea = () => {
+    return estudiantes.reduce((acc, est) => {
+        acc[est.nivel] = (acc[est.nivel] || 0) + 1;
+        return acc;
+    }, {});
+};
+
+export const promedioDeCadaMateriaPorArea = () => {
+    const materiasPorArea = {};
+
+    estudiantes.forEach(est => {
+        if (!materiasPorArea[est.nivel]) {
+            materiasPorArea[est.nivel] = {};
+        }
+
+        Object.keys(est.calificaciones).forEach(materia => {
+            if (!materiasPorArea[est.nivel][materia]) {
+                materiasPorArea[est.nivel][materia] = [];
+            }
+            materiasPorArea[est.nivel][materia].push(est.calificaciones[materia]);
+        });
+    });
+
+    Object.keys(materiasPorArea).forEach(area => {
+        Object.keys(materiasPorArea[area]).forEach(materia => {
+            const calificaciones = materiasPorArea[area][materia];
+            materiasPorArea[area][materia] = calificaciones.reduce((acc, nota) => acc + nota, 0) / calificaciones.length;
+        });
+    });
+
+    return materiasPorArea;
+};
